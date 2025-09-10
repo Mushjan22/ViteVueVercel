5a61a121ff3fa8cf05f1d1d1efab9275bc9eb5a9
Ani Harutyunyan <ani@slfstrg.com>
2025-09-10 11:08:44 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base:process.env.VITE_BASE_PATH || '/ViteVueVercel',
   test: {
     globals: true,
     environment: "jsdom",
